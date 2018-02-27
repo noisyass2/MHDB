008ac7a8e8c8789ae836987e9f5974e495f1c1cc
Bernard Rivera <bernard.rivera.m@gmail.com>
2018-02-27 16:54:25 +0800

    removed platforms/ completely

--- platforms/android/app/src/main/assets/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { SharedService } from "./shared/services/shared.service";
-
-@Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html",
-    providers: [SharedService]
-})
-export class AppComponent { }
